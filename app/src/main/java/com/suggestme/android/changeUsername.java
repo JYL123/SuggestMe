403f20885e46622cfad98ba8db3ce5f5a87f7abb
DESKTOP-7SUIEBU\Jasmine <jgreenteaj@gmail.com>
2017-07-22 15:24:47 +0800

    Update files

--- app/src/main/java/com/suggestme/android/changeUsername.java
+++ app/src/main/java/com/suggestme/android/changeUsername.java
@@ -27,14 +27,14 @@ public class changeUsername extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         final String email = getIntent().getExtras().getString("email");
         final String username = getIntent().getExtras().getString("username");
